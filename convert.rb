dd6b9b415e531a4b90ca425a3a29750134e872ed
Simone <hello@simonewebdesign.it>
2013-06-29 19:40:05 +0200

    Bug fix

--- convert.rb
+++ convert.rb
@@ -1,7 +1,6 @@
-path = ARGV[0]
-text = File.open(path).read
-#text.scan(/\[(.+?)\]\((.+?)\)/) { |m| puts "#{m.inspect}, 1st capture: #{$1}" }
-text.gsub!(/\[(.+?)\]\((.+?)\)/) { '<a href="' +$2+ '" target="_blank">' +$1+
+filename = ARGV[0]
+text = File.open(filename).read
+text.gsub!(/\[(.+)\]\((.+)\)/) { '<a href="' + $2 + '" target="_blank">' + $1 +
 '</a>' }
 File.new("#{ARGV[0]}.html", 'w').write(text)
 puts ARGV[0] + " successfully converted to " + ARGV[0] + ".html"
